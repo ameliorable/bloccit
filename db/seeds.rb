f5b67d95fa878e17ca299106ca490ed49e816761
Carolyn Walcott <carolynoliveira515@gmail.com>
2017-10-05 18:00:22 -0400

    Assignment 17: Rails: Seeding Data - Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,14 @@ posts = Post.all
   )
 end
 
+puts "#{Post.count}"
+Post.find_or_create_by!(title: "Floopy Doops", body: "Taco cat! Go hang a salami, I'm a lasagna hog!")
+puts "#{Post.count}"
+
+puts "#{Comment.count}"
+Comment.find_or_create_by!(body: "Werewolf Bar Mitzvah! Spooky, Scary!")
+puts "#{Comment.count}"
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
